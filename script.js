99a49fa5ff3e1f9cdf2e74622f1040b711538bf0
Shreyas K R <shreyaskr1022@gmail.com>
2022-05-07 18:47:21 +0530

    Basic keyboard functionality

---
+++ script.js
@@ -0,0 +1,84 @@
+let upperCase = true;
+let upperCase_shift = true;
+let shiftToggle = false;
+
+let content = document.getElementById("some-input");
+
+let all_keys = document.querySelectorAll(".keyboard__btn");
+
+let alphabets = document.querySelectorAll(".alphabet > .front > div");
+
+let caps_indicator = document.querySelector("#key-caps > .front > .indicator");
+
+let shift_indicator_l = document.querySelector(
+  "#key-shift-l > .front > .indicator"
+);
+
+let shift_indicator_r = document.querySelector(
+  "#key-shift-r > .front > .indicator"
+);
+
+const toggleCase = () => {
+  if (upperCase) {
+    alphabets.forEach((ele, index) => {
+      ele.childNodes[0].nodeValue = ele.childNodes[0].nodeValue.toLowerCase();
+    });
+  } else {
+    alphabets.forEach((ele, index) => {
+      ele.childNodes[0].nodeValue = ele.childNodes[0].nodeValue.toUpperCase();
+    });
+  }
+  upperCase = !upperCase;
+  caps_indicator.classList.toggle("indicator-off");
+};
+
+const shiftTogglerFn = () => {
+  shiftToggle = !shiftToggle;
+  shift_indicator_l.classList.toggle("indicator-off");
+  shift_indicator_r.classList.toggle("indicator-off");
+};
+
+const deleteCharacter = () => {
+  content.value = content.value.slice(0, -1);
+};
+
+const inputText = (ele) => {
+  console.log(ele);
+};
+
+all_keys.forEach((ele, index) => {
+  switch (ele.id) {
+    case "key-caps": {
+      ele.addEventListener("click", toggleCase);
+      break;
+    }
+    case "key-shift-l": {
+      ele.addEventListener("click", shiftTogglerFn);
+    }
+    case "key-shift-r": {
+      ele.addEventListener("click", shiftTogglerFn);
+      break;
+    }
+    case "key-delete": {
+      ele.addEventListener("click", deleteCharacter);
+      break;
+    }
+    case "key-tab": {
+      ele.addEventListener("click", () => {
+        content.value += "\t";
+      });
+      break;
+    }
+    default: {
+      ele.addEventListener("click", () => {
+        shiftToggle
+          ? (content.value += ele.querySelector(
+              " .front > div:first-child"
+            ).innerText)
+          : (content.value += ele.querySelector(
+              ".front > div:last-child"
+            ).innerText);
+      });
+    }
+  }
+});
