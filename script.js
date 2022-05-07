aa8dc02b9cb8dc229cf5280f01865236e4eafaa1
Shreyas K R <shreyaskr1022@gmail.com>
2022-05-08 02:24:32 +0530

    Add keypress sound and drawer+animations 😄

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ let alphabets = document.querySelectorAll(".alphabet > .front > div");
 
 let caps_indicator = document.querySelector("#key-caps > .front > .indicator");
 
+let body = document.querySelector("body");
+
+let textarea = document.querySelector(".textarea-container");
+
+let closeButton = document.getElementById("close-button");
+
+let overlay = document.getElementById("overlay");
+
 let shift_indicator_l = document.querySelector(
   "#key-shift-l > .front > .indicator"
 );
@@ -17,6 +25,12 @@ let shift_indicator_l = document.querySelector(
 let shift_indicator_r = document.querySelector(
   "#key-shift-r > .front > .indicator"
 );
+
+let drawer = document.getElementById("drawer");
+
+let drawerStatus = false;
+
+var audio = new Audio("click.wav");
 
 const shiftCaseChange = () => {
   if (upperCase_shift) {
@@ -29,6 +43,7 @@ const shiftCaseChange = () => {
     });
   }
   upperCase_shift = !upperCase_shift;
+  audio.play();
 };
 
 const toggleCase = () => {
@@ -47,6 +62,7 @@ const toggleCase = () => {
     shiftCaseChange();
   }
   caps_indicator.classList.toggle("indicator-off");
+  audio.play();
 };
 
 const shiftTogglerFn = () => {
@@ -54,10 +70,13 @@ const shiftTogglerFn = () => {
   shiftCaseChange();
   shift_indicator_l.classList.toggle("indicator-off");
   shift_indicator_r.classList.toggle("indicator-off");
+  audio.play();
 };
 
 const deleteCharacter = () => {
   content.value = content.value.slice(0, -1);
+  content.focus();
+  audio.play();
 };
 
 all_keys.forEach((ele, index) => {
@@ -80,6 +99,67 @@ all_keys.forEach((ele, index) => {
     case "key-tab": {
       ele.addEventListener("click", () => {
         content.value += "\t";
+        content.focus();
+        audio.play();
+      });
+      break;
+    }
+    case "key-return": {
+      ele.addEventListener("click", () => {
+        content.value += "\n";
+        content.focus();
+        audio.play();
+      });
+    }
+    case "key-command-l": {
+    }
+    case "key-command-r": {
+    }
+    case "key-option-r": {
+    }
+    case "key-option-l": {
+    }
+    case "key-control": {
+    }
+    case "key-function": {
+      ele.addEventListener("click", () => {
+        content.value += "";
+        content.focus();
+        audio.play();
+      });
+
+      break;
+    }
+    case "up-arrow": {
+      ele.addEventListener("click", () => {
+        audio.play();
+        content.focus();
+      });
+    }
+    case "down-arrow": {
+      ele.addEventListener("click", () => {
+        audio.play();
+        content.focus();
+      });
+    }
+    case "left-arrow": {
+      ele.addEventListener("click", () => {
+        audio.play();
+        content.focus();
+      });
+    }
+    case "right-arrow": {
+      ele.addEventListener("click", () => {
+        audio.play();
+        content.focus();
+      });
+      break;
+    }
+    case "key-space": {
+      ele.addEventListener("click", () => {
+        content.value += " ";
+        content.focus();
+        audio.play();
       });
       break;
     }
@@ -93,7 +173,21 @@ all_keys.forEach((ele, index) => {
               ".front > div:last-child"
             ).innerText);
         content.focus();
+        audio.play();
       });
     }
   }
+});
+
+closeButton.addEventListener("click", () => {
+  drawer.classList.remove("drawer--open");
+  body.style.justifyContent = "center";
+  textarea.style.marginBottom = "20px";
+  audio.play();
+});
+
+content.addEventListener("click", () => {
+  drawer.classList.add("drawer--open");
+  // body.style.justifyContent = "space-around";
+  textarea.style.marginBottom = "360px";
 });
